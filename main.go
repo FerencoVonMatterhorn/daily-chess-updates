47a19bc1abadd357d6e379a0ba65bf847e3fa81f
Ferenc Horvay <ferenc.horvay@de.markant.com>
2023-07-30 13:11:59 +0200

    feat(): add getAllGamesForDate

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/charmbracelet/log"
 	"io"
-	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,63 +14,101 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(response.Body)
 
 	if response.StatusCode < 200 || response.StatusCode >= 300 {
 		log.Fatalf("Unexpected response status code: %s", response.Status)
 	}
 
-	responseData, err := io.ReadAll(response.Body)
+	b, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error decoding API response: %s", err)
 	}
 
-	var monthlyChessGames MonthlyChessGames
-	json.Unmarshal(responseData, &monthlyChessGames)
+	// Create a variable to hold the API response data
+	var games Games
 
-	for _, game := range monthlyChessGames.Games {
-		fmt.Printf("White: %s\n", game.White.Username)
-		fmt.Printf("Black: %s\n", game.Black.Username)
-		if game.White.Result == "win" {
-			fmt.Printf("Winner: %s with a rating of: %d\n", game.White.Username, game.White.Rating)
-		} else {
-			fmt.Printf("Winner: %s with a rating of: %d\n", game.Black.Username, game.Black.Rating)
-		}
-		fmt.Println()
+	// Decode the API response JSON into the games variable
+	err = json.Unmarshal(b, &games)
+	if err != nil {
+		log.Fatalf("Error decoding API response: %s", err)
+	}
+
+	// Print the list of games
+	for _, game := range getAllGamesForDate(games.Games, 30, time.July, 2023) {
+		printBasicInformation(game)
 	}
 }
 
-// A MonthlyChessGames contains each game a user played in a given Month.
-type MonthlyChessGames struct {
-	Games []struct {
-		URL          string `json:"url"`
-		Pgn          string `json:"pgn"`
-		TimeControl  string `json:"time_control"`
-		EndTime      int    `json:"end_time"`
-		Rated        bool   `json:"rated"`
-		Tcn          string `json:"tcn"`
-		UUID         string `json:"uuid"`
-		InitialSetup string `json:"initial_setup"`
-		Fen          string `json:"fen"`
-		TimeClass    string `json:"time_class"`
-		Rules        string `json:"rules"`
-		White        struct {
-			Rating   int    `json:"rating"`
-			Result   string `json:"result"`
-			ID       string `json:"@id"`
-			Username string `json:"username"`
-			UUID     string `json:"uuid"`
-		} `json:"white"`
-		Black struct {
-			Rating   int    `json:"rating"`
-			Result   string `json:"result"`
-			ID       string `json:"@id"`
-			Username string `json:"username"`
-			UUID     string `json:"uuid"`
-		} `json:"black"`
-		Accuracies struct {
-			White float64 `json:"white"`
-			Black float64 `json:"black"`
-		} `json:"accuracies,omitempty"`
-	} `json:"games"`
+// printBasicInformation prints some basic information about a game (debugging purposes)
+func printBasicInformation(game Game) {
+	log.Infof("Game played at: %v", getTimeForUnixTimestamp(game.EndTime))
+	log.Infof("White: %s", game.White.Username)
+	log.Infof("Black: %s", game.Black.Username)
+	if game.White.Result == "win" {
+		log.Infof("Winner: %s with a rating of: %d", game.White.Username, game.White.Rating)
+	} else {
+		log.Infof("Winner: %s with a rating of: %d", game.Black.Username, game.Black.Rating)
+	}
+	fmt.Println()
+}
+
+// getAllGamesForDate returns an empty array or an array of games for a given date.
+func getAllGamesForDate(games []Game, day int, month time.Month, year int) []Game {
+	var gamesWithGivenDate []Game
+	for _, game := range games {
+		gameEndTime := getTimeForUnixTimestamp(game.EndTime)
+		if gameEndTime.Day() == day && gameEndTime.Month() == month && gameEndTime.Year() == year {
+			gamesWithGivenDate = append(gamesWithGivenDate, game)
+		}
+	}
+	return gamesWithGivenDate
+}
+
+// getTimeForUnixTimestamp is a simple wrapper for time.Unix() to safe some code while converting Unix Timestamps to Time Objects
+func getTimeForUnixTimestamp(unixTimeStamp int) time.Time {
+	return time.Unix(int64(unixTimeStamp), 0)
+}
+
+// Games contains an array of Game
+type Games struct {
+	Games []Game `json:"games"`
+}
+
+// Game holds all given Information about a Game no chessCom.com
+type Game struct {
+	URL          string `json:"url"`
+	Pgn          string `json:"pgn"`
+	TimeControl  string `json:"time_control"`
+	EndTime      int    `json:"end_time"`
+	Rated        bool   `json:"rated"`
+	Tcn          string `json:"tcn"`
+	UUID         string `json:"uuid"`
+	InitialSetup string `json:"initial_setup"`
+	Fen          string `json:"fen"`
+	TimeClass    string `json:"time_class"`
+	Rules        string `json:"rules"`
+	White        struct {
+		Rating   int    `json:"rating"` // Represents the Rating after the game is finished
+		Result   string `json:"result"` // Can be: "win", "resigned", "checkmated", "repetition", "stalemate", "timeout"
+		ID       string `json:"@id"`
+		Username string `json:"username"`
+		UUID     string `json:"uuid"`
+	} `json:"white"`
+	Black struct {
+		Rating   int    `json:"rating"`
+		Result   string `json:"result"`
+		ID       string `json:"@id"`
+		Username string `json:"username"`
+		UUID     string `json:"uuid"`
+	} `json:"black"`
+	Accuracies struct {
+		White float64 `json:"white"`
+		Black float64 `json:"black"`
+	} `json:"accuracies,omitempty"`
 }
